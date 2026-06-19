58c462f4a65593c828a268af41db750b8014cb0a
Shashidhar020 <shashidharab736@gmail.com>
2026-06-19 18:25:37 +0530

    backend updated

--- dms_backend/src/config/database.js
+++ dms_backend/src/config/database.js
@@ -1,14 +1,22 @@
 import mysql from 'mysql2/promise';
 import { env } from './env.js';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const isDocker = process.env.RUNNING_IN_DOCKER === "true";
 
 export const db = mysql.createPool({
   host: env.dbHost,
   user: env.dbUser,
-  password: '',
+  password: env.dbPassword,
   database: env.dbName,
   ssl: {
-    ca: fs.readFileSync("./ca.pem"),
+    ca: fs.readFileSync(path.join(__dirname, "ca.pem"))
   },
   port: env.dbPort,
   waitForConnections: true,
