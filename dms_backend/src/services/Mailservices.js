961b8c7de20aa7d8bc006bdfd9bbe3c779ae7c33
Shashidhar020 <shashidharab736@gmail.com>
2026-06-20 15:19:16 +0530

    backend updated

--- dms_backend/src/services/Mailservices.js
+++ dms_backend/src/services/Mailservices.js
@@ -18,22 +18,23 @@ dotenv.config();
 //   },
 // });
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
 // const transporter = nodemailer.createTransport({
-//   service: "gmail",
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   family: 4,
 //   auth: {
 //     user: process.env.EMAIL_USER,
 //     pass: process.env.EMAIL_PASS,
 //   },
 // });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 // Verify SMTP connection
 transporter.verify((error, success) => {
   if (error) {
